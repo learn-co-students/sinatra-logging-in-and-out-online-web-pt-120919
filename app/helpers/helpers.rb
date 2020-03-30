f5ace7d9cd35d82678f357343acc0b7b857c40d3
hnnav <henna.v@icloud.com>
2020-03-30 14:18:02 +0300

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+    def self.current_user(session)
+        # accept the session hash as an argument
+        # use the `user_id` from the session hash to find the user
+        # return that user
+        User.find_by_id(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+    end
 end
